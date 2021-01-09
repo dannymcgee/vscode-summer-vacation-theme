ecc70ebb3fc62d1f5d10a03cbb9a1df520924081
Danny McGee <dmcgee@insiten.com>
2021-01-09 14:22:00 -0500

    Tweak light palette for contrast, legibility

--- src/colors/light-neutrals.js
+++ src/colors/light-neutrals.js
@@ -1,7 +1,7 @@
 const neutrals = {
-	gray100: '#3D4143',
-	gray200: '#647781',
-	gray300: '#929EA5',
+	gray100: '#222526',
+	gray200: '#556F7D',
+	gray300: '#7B909E',
 	gray400: '#BFC5CF',
 	gray500: '#E3E7ED',
 	gray600: '#EFF2F5',
--- src/colors/light-palette.js
+++ src/colors/light-palette.js
@@ -2,12 +2,12 @@ const colors = {
 	red:       '#FF413E',
 	orange:    '#FF5C00',
 	yellow:    '#FF8C00',
-	teal:      '#00C7A7',
+	teal:      '#00A3B2',
 	green:     '#00AC45',
-	blueDark:  '#008AF5',
 	blueLight: '#96C5E9',
+	blueDark:  '#008AF5',
 	purple:    '#8E62FF',
-	pinkLight: '#FF738F',
+	pinkLight: '#FA5F71',
 	pinkDark:  '#FF2070',
 }
 
