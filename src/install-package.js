8866a19490058a8c456745e75035caff65738442
Danny McGee <dannymcgee@gmail.com>
2021-12-08 18:36:17 -0500

    Remove shelljs dependency; Upgrade vsce

--- src/install-package.js
+++ src/install-package.js
@@ -1,9 +1,49 @@
+const cp = require('child_process')
 const path = require('path')
-const shell = require('shelljs')
 const package = require('../package.json')
 
 const absPath = path.resolve('./')
 const version = package.version
 const name = package.name
 
-shell.exec(`code --install-extension "${absPath}\\${name}-${version}.vsix"`)
+;(async function () {
+	try {
+		await spawn("code", [
+			"--install-extension",
+			`"${absPath}\\${name}-${version}.vsix"`,
+		])
+	} catch (err) {
+		if (typeof err === 'number')
+			process.exit(err)
+		else {
+			console.error(err)
+			process.exit(1)
+		}
+	}
+
+	process.exit(0)
+})()
+
+/**
+ * @param {string} cmd
+ * @param {string[]} args
+ *
+ * @returns {Promise<void>}
+ */
+function spawn(cmd, args) {
+	return new Promise((resolve, reject) => {
+		let proc = cp.spawn(cmd, args, {
+			shell: true,
+			stdio: "inherit",
+		})
+
+		proc.on("close", onExit)
+		proc.on("exit", onExit)
+		proc.on("error", reject)
+
+		function onExit(code = 0) {
+			if (code) reject(code)
+			else resolve()
+		}
+	})
+}
